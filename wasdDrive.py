ea5fafb92ba9230e69aade2bb62f970155c4be1d
jubeemer <jubeemer@umich.edu>
2018-05-17 15:00:37 +0200

    [serial] WASD event handling and sending

---
+++ wasdDrive.py
@@ -0,0 +1,35 @@
+import pygame, serial, math, sys
+from pygame.locals import *
+
+pygame.init()
+screen = pygame.display.set_mode((200, 200))
+
+class WASDcontroller:
+    def __init__(self, port_in):
+        self.start1 = 0xDA
+        self.start2 = 0xAD
+        self.ser = serial.Serial(port_in, 115200)
+        self.data = 0x00 # _ _ _ _ D A S W
+
+    def decode_input(self):
+        for event in pygame.event.get():
+            if not hasattr(event, 'key'): continue
+            if event.type == KEYDOWN:
+                if event.key == K_w: self.data |= 0x01
+                if event.key == K_s: self.data |= 0x02
+                if event.key == K_a: self.data |= 0x04
+                if event.key == K_d: self.data |= 0x08
+            if event.type == KEYUP:
+                if event.key == K_w: self.data &= 0xfe
+                if event.key == K_s: self.data &= 0xfd
+                if event.key == K_a: self.data &= 0xfb
+                if event.key == K_d: self.data &= 0xf7
+            print(self.data)
+            self.send_data(self.data)
+
+    def send_data(self, data):
+        self.ser.write(bytearray([self.start1, self.start2, data]))
+
+keyboard = WASDcontroller("COM8")
+while True:
+    keyboard.decode_input()
