fadb9a7fe4d69d7e83599260d3fdcb460a78b7c2
jubeemer <jubeemer@umich.edu>
2018-05-23 13:59:50 +0200

    [pid] constant time, tuned pid with 0.9 base speed, no if-else

--- pid_control_drive/main.cpp
+++ pid_control_drive/main.cpp
@@ -10,10 +10,10 @@ Timer timer;
 
 float t0, t1;
 float dt = .001;
+bool first_loop = 1;
 
-float BASE_SPEED = 0.8;
-float OUTER_MOTOR_SPEED = BASE_SPEED;
-float INNER_MOTOR_SPEED;
+float BASE_SPEED = 0.9;
+float LEFT_SPEED, RIGHT_SPEED;
 
 float line_error = 0;
 float error_prev = 0;
@@ -23,9 +23,9 @@ float derivative = 0;
 float correction = 0;
 
 // CLOSED-LOOP CONTROL PARAMETERS
-float KP = 1.2;
+float KP = 1.0;
 float KI = 0;
-float KD = 0.015;
+float KD = 0.01;
 
 int main() {
     timer.start();
@@ -33,13 +33,17 @@ int main() {
         
     float bat = robot.battery();
     robot.locate(0, 0);
-    robot.printf("%.3fbat");
+    robot.printf("%.3f", bat);
     
     robot.sensor_auto_calibrate();
     
     while(1) {
-        // START TIME
-        t0 = timer.read_ms();
+        if(first_loop) {
+            t0 = timer.read_ms();
+        }
+        else if(timer.read() < 0.008) {
+            continue;
+        }
         
         // CALCULATE ERROR TERMS
         line_error = robot.line_position();
@@ -47,26 +51,27 @@ int main() {
         derivative = (line_error - error_prev) / dt;
         
         // ERROR ADJUSTMENT
-        correction = abs(KP*line_error + KI*integral + KD*derivative);
-        INNER_MOTOR_SPEED = OUTER_MOTOR_SPEED - correction;
+        correction = KP*line_error + KI*integral + KD*derivative;
+        LEFT_SPEED = BASE_SPEED + correction;
+        RIGHT_SPEED = BASE_SPEED - correction;
         
-        // CHECK FOR BAD COMMANDS
-        if(INNER_MOTOR_SPEED > 1) {
-            INNER_MOTOR_SPEED = 1;
+        // CHECK BAD COMMANDS
+        if(LEFT_SPEED > 1) {
+            LEFT_SPEED = 1;
         }
-        if(INNER_MOTOR_SPEED < -1) {
-            INNER_MOTOR_SPEED = -1;
+        else if(LEFT_SPEED < -1) {
+            LEFT_SPEED = -1;
         }
-        
+        if(RIGHT_SPEED > 1) {
+            RIGHT_SPEED = 1;
+        }
+        else if(RIGHT_SPEED < -1) {
+            RIGHT_SPEED = -1;
+        }
+
         // SET MOTORS
-        if(line_error < 0) {
-            robot.left_motor(INNER_MOTOR_SPEED);
-            robot.right_motor(OUTER_MOTOR_SPEED);
-        }
-        else {
-            robot.left_motor(OUTER_MOTOR_SPEED);
-            robot.right_motor(INNER_MOTOR_SPEED);
-        }
+        robot.left_motor(LEFT_SPEED);
+        robot.right_motor(RIGHT_SPEED);
         
         // UPDATE ERROR
         error_prev = line_error;
@@ -74,10 +79,14 @@ int main() {
         // NECESSARY WAIT
         wait_ms(1);
         
-        // END TIME
-        t1 = timer.read_ms();
-        dt = (t1 - t0) / 1000.0;  
-    }
-            
+        // CALCULATE dt
+        if(first_loop) {
+            t1 = timer.read_ms();
+            dt = (t1 - t0) / 1000.0;
+            first_loop = 0;
+        }
         
+        // RESET TIMER
+        timer.reset();
+    }
 }
