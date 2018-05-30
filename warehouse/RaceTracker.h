b74361328686a2d776aa0f9b719c121a2ee7184b
jubeemer <jubeemer@umich.edu>
2018-05-30 14:13:33 +0200

    [warehouse] fixed spacing issues

--- warehouse/RaceTracker.h
+++ warehouse/RaceTracker.h
@@ -4,65 +4,65 @@
 
 class RaceTracker {
 public:
-	// Constructor for a RaceTracker.
-	// robot_in           : the robot which will be racing
-	// race_num_laps_in   : number of laps in the race
-	// black_tolerance_in : the tolerance band for what sensor values are 
-	//                      considered "black"
-	// white_tolerance_in : the tolerance band for what sensor values are
-	//                      considered "white"
-	RaceTracker(m3pi &robot_in, int race_num_laps_in,
-		int black_tolerance_in, int white_tolerance_in);
+    // Constructor for a RaceTracker.
+    // robot_in           : the robot which will be racing
+    // race_num_laps_in   : number of laps in the race
+    // black_tolerance_in : the tolerance band for what sensor values are 
+    //                      considered "black"
+    // white_tolerance_in : the tolerance band for what sensor values are
+    //                      considered "white"
+    RaceTracker(m3pi &robot_in, int race_num_laps_in,
+        int black_tolerance_in, int white_tolerance_in);
 
-	// Reads calibrated sensor values from the infrared sensors.
-	void get_raw_sensors();
+    // Reads calibrated sensor values from the infrared sensors.
+    void get_raw_sensors();
 
-	// Returns true if the robot's front edge is over a completely black surface
-	// (i.e., the start/finish line perpendicular to the track)
-	bool is_black_surface();
+    // Returns true if the robot's front edge is over a completely black surface
+    // (i.e., the start/finish line perpendicular to the track)
+    bool is_black_surface();
 
-	// Returns true if the robot's front edge is over a completely white surface
-	// (i.e., off the track entirely).
-	bool is_white_surface();
+    // Returns true if the robot's front edge is over a completely white surface
+    // (i.e., off the track entirely).
+    bool is_white_surface();
 
-	// Print to the LCD display a summary that includes the number of laps traversed,
-	// the time taken for each lap, and the total race time.
-	void print_summary();
+    // Print to the LCD display a summary that includes the number of laps traversed,
+    // the time taken for each lap, and the total race time.
+    void print_summary();
 
-	// Print to the LCD display a summary of the total race time for the warehouse
-	void print_summary(int ms);
+    // Print to the LCD display a summary of the total race time for the warehouse
+    void print_summary(int ms);
 
-	// Print a goodbye message to the user
-	void print_exit();
+    // Print a goodbye message to the user
+    void print_exit();
 
-	// Record the time of the previous lap and increment the lap counter.
-	void record_lap();
+    // Record the time of the previous lap and increment the lap counter.
+    void record_lap();
 
-	// Set the flag indicating whether the robot is currently over the start/finish line
-	void set_line_flag(bool flag);
+    // Set the flag indicating whether the robot is currently over the start/finish line
+    void set_line_flag(bool flag);
 
-	// Returns line_flag
-	bool get_line_flag();
+    // Returns line_flag
+    bool get_line_flag();
 
-	// Check whether the robot has traversed the required number of laps.
-	bool is_finished();
+    // Check whether the robot has traversed the required number of laps.
+    bool is_finished();
 
-	// Start the timer
-	void start_timer();
+    // Start the timer
+    void start_timer();
 
-	// check for intersections
-	bool is_corner();
+    // check for intersections
+    bool is_corner();
 
 private:
-	m3pi robot;
-	Timer lap_timer;
-	int race_num_laps;
-	bool line_flag;
-	int sensor_data[5];
-	vector<int> lap_times;
-	int num_laps;
-	int total_time;
+    m3pi robot;
+    Timer lap_timer;
+    int race_num_laps;
+    bool line_flag;
+    int sensor_data[5];
+    vector<int> lap_times;
+    int num_laps;
+    int total_time;
 
-	const int BLACK_TOLERANCE, WHITE_TOLERANCE;
+    const int BLACK_TOLERANCE, WHITE_TOLERANCE;
 };
 
--- warehouse/main.cpp
+++ warehouse/main.cpp
@@ -15,53 +15,53 @@ const int BLACK_TOLERANCE = 100;
 const int WHITE_TOLERANCE = 400;
 
 int main() {
-	int count = 0;
-	robot.cls();
-	robot.sensor_auto_calibrate();
-	// Music m;
+    int count = 0;
+    robot.cls();
+    robot.sensor_auto_calibrate();
+    // Music m;
 
-	Warehouse w(robot, bt);
-	PIDLineFollower controller(robot, .6, 0.70, 0, 0.012);
-	RaceTracker race(robot, 2, BLACK_TOLERANCE, WHITE_TOLERANCE);
+    Warehouse w(robot, bt);
+    PIDLineFollower controller(robot, .6, 0.70, 0, 0.012);
+    RaceTracker race(robot, 2, BLACK_TOLERANCE, WHITE_TOLERANCE);
 
-	timer.start();
-	
-	while (1) {
-		// Get any waypoints from bluetooth serial
-		w.get_station_path();
+    timer.start();
+    
+    while (1) {
+        // Get any waypoints from bluetooth serial
+        w.get_station_path();
 
-		// Process intersections in warehouse
-		race.get_raw_sensors();
-		if (race.is_corner() && !w.is_end()) {
-			++count;
-			robot.cls();
-			robot.locate(0, 0);
-			robot.printf("%d", count);
-			w.run();
-		}
+        // Process intersections in warehouse
+        race.get_raw_sensors();
+        if (race.is_corner() && !w.is_end()) {
+            ++count;
+            robot.cls();
+            robot.locate(0, 0);
+            robot.printf("%d", count);
+            w.run();
+        }
 
-		// Press button to kill robot
-		if (!mypin) {
-			robot.stop();
-			// m.play_fight_song_2(robot);
-			break;
-		}
-		
+        // Press button to kill robot
+        if (!mypin) {
+            robot.stop();
+            // m.play_fight_song_2(robot);
+            break;
+        }
+        
         // Don't move is queue is empty
        if (w.is_end()) {
-			robot.stop();
-			continue;
-		}
+            robot.stop();
+            continue;
+        }
 
-		// Wait until dt has passed to run the control algorithm
-		if (timer.read() < dt) {
-			continue;
-		}
+        // Wait until dt has passed to run the control algorithm
+        if (timer.read() < dt) {
+            continue;
+        }
 
         // Line following algorithm
-		controller.drive(dt);
+        controller.drive(dt);
 
-		timer.reset();
+        timer.reset();
 
-	}
+    }
 }
